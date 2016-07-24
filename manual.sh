2fb70043fcbd0df94f36f83828ed2566b013a62d
Margus Mahler <margus@toggl.com>
2016-07-24 16:26:16 +0300

    iteration 2 + manual script

--- manual.sh
+++ manual.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -o errexit # abort on first unsucessfull line
 
-BASEDIR=$(date +"%Y%m%d")
-SUBHOUR=$(date +"%H" --date="2 hour ago")
+BASEDIR=$1
+SUBHOUR=$2
 
 mkdir -p ${BASEDIR}/working/${SUBHOUR}00
 # set captions for images
@@ -25,12 +25,8 @@ for img in ${BASEDIR}/${SUBHOUR}00/*.jpg; do
   mv ${img} ${BASEDIR}/working/${SUBHOUR}00
 done
 
-echo "ffmpeg -pattern_type glob -i '${BASEDIR}/working/${SUBHOUR}00/*.jpg'  -c:v libx264 -vf scale=800:-1 ${BASEDIR}_${SUBHOUR}00.mp4" > ffmpeg_script.sh
+echo "ffmpeg -pattern_type glob -i '${BASEDIR}/working/${SUBHOUR}00/*.jpg'  -c:v libx264 -vf scale=800:-1 ${BASEDIR}_${SUBHOUR}00.mp4" > ffmpeg_manual_script.sh
 # quick view movie
-echo "ffmpeg -i ${BASEDIR}_${SUBHOUR}00.mp4 -filter:v 'setpts=0.25*PTS' ${BASEDIR}_${SUBHOUR}00_short.mp4" >> ffmpeg_script.sh
-chmod +x ffmpeg_script.sh
-./ffmpeg_script.sh
-
-# cycle image files, keeping last 2 days
-REMOVABLE_DIR=$(date +"%Y%m%d" --date="3 days ago")
-rm -fr ${REMOVABLE_DIR}
+echo "ffmpeg -i ${BASEDIR}_${SUBHOUR}00.mp4 -filter:v 'setpts=0.25*PTS' ${BASEDIR}_${SUBHOUR}00_short.mp4" >> ffmpeg_manual_script.sh
+chmod +x ffmpeg_manual_script.sh
+./ffmpeg_manual_script.sh
